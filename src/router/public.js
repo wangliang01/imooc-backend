7cb0c3292224484f9547e9b5bdf080b4e37f6cb3
王亮 <wangliang4@newhope.cn>
2024-08-06 22:59:15 +0800

    feat: 调整获取验证码接口

--- src/router/public.js
+++ src/router/public.js
@@ -2,8 +2,10 @@ import Router from 'koa-router'
 const router = new Router({
   prefix: '/api/public'
 })
-import globalContronller from '../api/public'
+import globalController from '../api/public'
 
-router.get('/captcha', globalContronller.getCaptchaV2)
+// Controller
+
+router.get('/captcha', globalController.getCaptcha)
 
 export default router
